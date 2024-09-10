00f2c48200bf6da23fc02b13118ae525da043c2b
Joel Hellberg <joehe737@student.liu.se>
2024-09-10 11:27:58 +0200

    First Commit

---
+++ Project - Global Illumination/Project - Global Illumination.cpp
@@ -0,0 +1,12 @@
+﻿// Project - Global Illumination.cpp : Defines the entry point for the application.
+//
+
+#include "Project - Global Illumination.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
