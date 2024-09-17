d08516e10cf5942207862a15a928264fad480c67
Joel Hellberg <joehe737@student.liu.se>
2024-09-17 13:55:38 +0200

    Lade till scene

---
+++ Project - Global Illumination/scene.cpp
@@ -0,0 +1,18 @@
+﻿// Project - Global Illumination.cpp : Defines the entry point for the application.
+//
+
+#include "Project - Global Illumination.h"
+#include <glm/glm.hpp>
+
+
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+
+	
+}
